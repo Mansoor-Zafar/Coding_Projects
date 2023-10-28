eedc3e287eb0c5af3f249ffd3d897e67fce9f2f3
Mansoor-Zafar <145301137+Mansoor-Zafar@users.noreply.github.com>
2023-10-28 12:03:29 -0400

    Delete Java directory

--- Java/AudioPlayer/Main.java
+++
@@ -1,9 +0,0 @@
-package fpg;
-
-public class Main {
-	public static void main(String[] args)
-	{	
-		AudioPlayerGUI gui = new AudioPlayerGUI("songs.txt");
-		gui.build();
-	}
-}
--- Java/AudioPlayer/readme.md
+++
@@ -1,1 +0,0 @@
-DL: https://drive.google.com/drive/folders/1JxH_5kBRY7hznDQQkSupUekGlsw8yCcZ?usp=sharing
