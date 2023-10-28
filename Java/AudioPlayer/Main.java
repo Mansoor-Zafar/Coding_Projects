293857eb4ba5326b1a90c2609c29b29a74ffaca5
Mansoor-Zafar <145301137+Mansoor-Zafar@users.noreply.github.com>
2023-10-27 20:04:54 -0400

    Update Main.java

--- Java/AudioPlayer/Main.java
+++ Java/AudioPlayer/Main.java
@@ -3,7 +3,6 @@ package fpg;
 public class Main {
 	public static void main(String[] args)
 	{	
-		System.out.println("hi");
 		AudioPlayerGUI gui = new AudioPlayerGUI("songs.txt");
 		gui.build();
 	}
