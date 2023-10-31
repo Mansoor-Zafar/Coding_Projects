3098c0e6b7b9b0f67e5bd2c1e815ce9d863b3309
Mansoor-Zafar <145301137+Mansoor-Zafar@users.noreply.github.com>
2023-10-30 23:23:54 -0400

    Create ClockGUI.java

---
+++ TimeClock/ClockGUI.java
@@ -0,0 +1,43 @@
+package fpg;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class ClockGUI{
+
+	private JFrame window;
+	private JLabel time;
+	private Clock clock = new Clock();
+	
+	public void build()
+	{
+		this.window = new JFrame("Clock.exe");
+		this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.window.setSize(706,336);
+		this.window.setResizable(false);
+		this.window.setLayout(null);
+		
+		
+		this.window.getContentPane().setBackground(Color.gray);
+		
+		this.time = new JLabel("Getting time...");
+		this.time.setFont(new Font("Cascadia Code", Font.PLAIN, 68));
+		this.time.setBounds(70,45,577,198);
+		
+		this.window.add(time);
+		this.window.setLocationRelativeTo(null);
+		this.window.setVisible(true);
+		
+		this.setTime();
+		
+	}
+	
+	private void setTime()
+	{
+		clock.setTime(this.time);
+		
+	}
+
+}
